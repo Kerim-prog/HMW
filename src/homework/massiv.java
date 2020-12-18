3e079d4f979623b7a8742bf50db73e638a9c21a3
kerim-prog <akram_6@mail.ru>
2020-12-18 18:19:15 +0400

    first

---
+++ src/homework/massiv.java
@@ -0,0 +1,28 @@
+package homework;
+
+import java.util.Scanner;
+
+public class massiv {
+    public static void main(String[] args) {
+        int arr[] = {2, 7, 9,4,0};
+
+        System.out.println("Ədədi daxil edin");
+        Scanner sc = new Scanner(System.in);
+        int temp = sc.nextInt();
+
+        for(int a:arr)
+        {
+            if(temp==a)
+            {
+                System.out.println("Daxil etdiyiniz eded var");
+            }
+
+            else
+                System.out.println("Daxil etdiyiniz eded yoxdur");
+
+
+        }
+
+
+    }
+}
