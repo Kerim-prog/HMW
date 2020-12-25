8d74a85a7be7675efc28a3e018de8321a4466854
kerim-prog <akram_6@mail.ru>
2020-12-25 17:39:03 +0400

    faktorial last

--- src/homework/faktorial.java
+++ src/homework/faktorial.java
@@ -2,16 +2,13 @@ package homework;
 
 public class faktorial {
     public static void main(String[] args) {
-        System.out.println(findFaktorial(5));
+        System.out.println(findFaktorial(6));
 
     }
 
     public static int findFaktorial(int a)
     {
-        int fak=1;
-        if(a!=1){
-        fak=a*findFaktorial(a-1);
-        }
-                return fak;
+        if(a!=1){return a*findFaktorial(a-1);}
+        else return 1;
     }
 }
