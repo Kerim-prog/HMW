5075a7230ddef18a625e6ce28b7e03ea4913f56b
Kerim-prog <akram_6"mail.ru>
2021-01-22 19:17:32 +0400

    actualmaxdate

--- src/homework/day/nine/DateExercise.java
+++ src/homework/day/nine/DateExercise.java
@@ -4,8 +4,11 @@ import java.util.*;
 public class DateExercise {
     public static void main(String[] args) {
         Calendar cal = Calendar.getInstance();
-        System.out.println();
-        System.out.println("\nCurrent Date and Time:" + cal.getTime());
+        System.out.println("Current Date and Time:" + cal.getTime());
+        System.out.println("Actual Max Year:" + cal.getActualMaximum(Calendar.YEAR));
+        System.out.println("Actual Max Month:" + cal.getActualMaximum(Calendar.MONTH));
+        System.out.println("Actual Week of year:" + cal.getActualMaximum(Calendar.WEEK_OF_YEAR));
+        System.out.println("Actual Max Date:" + cal.getActualMaximum(Calendar.DATE));
 
     }
 }
--- src/lesson/collections/CollectionExample1.java
+++ src/lesson/collections/CollectionExample1.java
@@ -3,19 +3,30 @@ import java.util.*;
 
 public class CollectionExample1 {
     public static void main(String[] args) {
-        List<String> animals = new ArrayList<String>();
-        animals.add("Dog");
-        animals.add("Cat");
-        animals.add("Elepphant");
-        animals.add("Monkey");
-        animals.add("Jerapah");
+        List<String> ls = new ArrayList<String>();
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("string");
+        arrayList.add(23);
 
-        animals.remove(3);
+        Set<String> set = new LinkedHashSet<>();
+        String a = "abc";
+        String b = "abc";
+        String c = new String("abc");
+        set.add(a);
+        set.add(c);
+        System.out.println(set);
 
-        for(int i=0;i<animals.size();i++)
-        {
-            System.out.println(animals.get(i));
-        }
+        /*
+        Map<Integer,Person> map = new HashMAp<>();
+        map.put(1,person1);
+        map.put(2,person2);
+
+        map.forEach((key,value)->{
+        if(key==3)
+        System.out.println(value);
+        });
+         */
+
 
     }
 }
